73a60896a56f130c228129d762c5d8031738fefe
SupportEngine <support@elonovo.com>
2026-02-09 14:26:43 -0800

    Add files via upload

---
+++ language/english/nameguard_lang.php
@@ -0,0 +1,11 @@
+<?php
+
+$lang = [
+    'nameguard_module_name' => 'NameGuard',
+    'nameguard_module_description' => 'Blocks suspicious mixed-case screen names during registration',
+
+    // Error messages
+    'nameguard_suspicious_name' => 'Please use a normal name format.',
+    'nameguard_gibberish_name' => 'Please use a real name or nickname.',
+    'nameguard_unreadable_name' => 'Please use a readable screen name.',
+];
