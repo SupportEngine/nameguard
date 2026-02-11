bf280cd6dbbe9760a56d42536df3df34d50651dc
SupportEngine <support@elonovo.com>
2026-02-11 08:48:18 -0800

    Update upd.nameguard.php

--- upd.nameguard.php
+++ upd.nameguard.php
@@ -2,7 +2,7 @@
 
 class Nameguard_upd
 {
-    public $version = '1.0.0';
+    public $version = '1.1.0';
 
     /**
      * Install the add-on
@@ -30,6 +30,17 @@ class Nameguard_upd
             'enabled' => 'y'
         ]);
 
+        // Also hook into registration start for diagnostic confirmation
+        ee()->db->insert('extensions', [
+            'class' => 'Nameguard_ext',
+            'method' => 'on_register_start',
+            'hook' => 'member_member_register_start',
+            'settings' => '',
+            'priority' => 5,
+            'version' => $this->version,
+            'enabled' => 'y'
+        ]);
+
         return true;
     }
 
@@ -43,7 +54,7 @@ class Nameguard_upd
         // Remove the module
         ee()->db->where('module_name', 'Nameguard')->delete('modules');
 
-        // Remove the extension
+        // Remove all extension hooks
         ee()->db->where('class', 'Nameguard_ext')->delete('extensions');
 
         return true;
@@ -59,6 +70,24 @@ class Nameguard_upd
     {
         if (version_compare($current, $this->version, '==')) {
             return false;
+        }
+
+        // If upgrading from 1.0.x, register the new diagnostic hook
+        if (version_compare($current, '1.1.0', '<')) {
+            ee()->db->insert('extensions', [
+                'class' => 'Nameguard_ext',
+                'method' => 'on_register_start',
+                'hook' => 'member_member_register_start',
+                'settings' => '',
+                'priority' => 5,
+                'version' => $this->version,
+                'enabled' => 'y'
+            ]);
+
+            // Update existing hook version
+            ee()->db->where('class', 'Nameguard_ext')
+                ->where('hook', 'member_member_register_errors')
+                ->update('extensions', ['version' => $this->version]);
         }
 
         return true;
