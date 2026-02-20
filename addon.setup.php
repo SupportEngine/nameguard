057d11f1930760fbb98aae122630d182fef23f9b
SupportEngine <support@elonovo.com>
2026-02-20 09:06:59 -0800

    Update addon.setup.php

--- addon.setup.php
+++ addon.setup.php
@@ -5,7 +5,7 @@ return [
     'author_url' => 'https://www.elonovo.com',
     'name' => 'NameGuard',
     'description' => 'Blocks suspicious mixed-case screen names and usernames during registration.',
-    'version' => '1.1.0',
+    'version' => '1.1.1',
     'namespace' => 'Elonovo\Nameguard',
     'settings_exist' => false,
 ];
